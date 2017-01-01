3a11327071221f720871d0ae7b97c80fe66303a7
Rachael Alexanderson <eruanna@drdteam.org>
2017-01-01 09:39:37 -0500

    - Version q1.2.0

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "1.2pre"
+#define VERSIONSTR "1.1.0"
 
 // The version as seen in the Windows resource
 #define RC_FILEVERSION 1,1,9999,0
 #define RC_PRODUCTVERSION 1,1,9999,0
-#define RC_PRODUCTVERSION2 "1.2pre"
+#define RC_PRODUCTVERSION2 "1.1.0"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
