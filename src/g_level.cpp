9c35e736fbec6b9efe2abbc2d775e71467033d62
Rachael Alexanderson <eruanna@drdteam.org>
2017-02-16 20:39:35 -0500

    - issuing "control #" now changes spycam to selected player

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -2016,7 +2016,11 @@ CCMD(control)
 			StatusBar->AttachToPlayer (players[consoleplayer].camera->player);
 			StatusBar->ShowPlayerName ();
 #endif
-			playerfornode[0] = std::stoi(argv[1]) - 1;
+			int pnum = std::stoi(argv[1]) - 1;
+			players[consoleplayer].camera = players[pnum].mo;
+			S_UpdateSounds(players[consoleplayer].camera);
+			StatusBar->AttachToPlayer (&players[pnum]);
+			playerfornode[0] = pnum;
 		}
 		else
 			Printf("No player %d in game!\n", std::stoi(argv[1]));
