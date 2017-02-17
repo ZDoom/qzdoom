4170554caa44a0883b8c2f3dde4e58fd1503c10e
Rachael Alexanderson <eruanna@drdteam.org>
2017-02-16 22:30:48 -0500

    - allow "control #" outside of a level. This makes it useable during intermission screens, required in order to send the "ready" from all players.

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -1988,11 +1988,6 @@ extern int nodeforplayer[];
 extern int playerfornode[];
 CCMD(control)
 {
-	if (gamestate != GS_LEVEL)
-	{
-		Printf ("Must be in game!\n");
-		return;
-	}
 	if (netgame)
 	{
 		Printf ("Cannot use this in netgame!\n");
@@ -2009,10 +2004,13 @@ CCMD(control)
 		{
 			int pnum = std::stoi(argv[1]) - 1;
 			Net_Arbitrator = doomcom.consoleplayer = consoleplayer = playerfornode[0] = pnum;
-			S_UpdateSounds(players[consoleplayer].camera);
-			StatusBar->AttachToPlayer (players[consoleplayer].camera->player);
-			players[consoleplayer].SendPitchLimits();
-			StatusBar->ShowPlayerName ();
+			if (gamestate == GS_LEVEL)
+			{
+				S_UpdateSounds(players[consoleplayer].camera);
+				StatusBar->AttachToPlayer (players[consoleplayer].camera->player);
+				players[consoleplayer].SendPitchLimits();
+				StatusBar->ShowPlayerName ();
+			}
 		}
 		else
 			Printf("No player %d in game!\n", std::stoi(argv[1]));
